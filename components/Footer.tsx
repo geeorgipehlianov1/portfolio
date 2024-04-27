fdc4b5ef7e3c84d26185ec1632a9c65fe8f6998f
Georgi Pehlivanov <george9040@abv.bg>
2024-04-27 22:47:49 +0300

    Add new colors for bg footer and header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='mt-20 flex h-60 flex-col items-center justify-center gap-5 border-t bg-[#00bcd4]'>
+    <footer className='mt-20 flex h-60 flex-col items-center justify-center gap-5 border-t bg-green-700'>
       <p className='w-[600px] text-center'>
         If you wait for motivation, you’re doing it wrong. The formula to induce
         motivation starts with action, which sparks inspiration, which sparks
